42ae48f96ff82338d5371cb7812669185a413b5f
Malak Tayeh <malaktayeh22@gmail.com>
2019-03-10 17:51:19 -0400

    added getResult function

--- src/App.js
+++ src/App.js
@@ -24,16 +24,79 @@ class App extends Component {
   input = (event) => {
     // save input in input variable
     const input = event.target.innerHTML;
-    console.log(input);
 
     if (event.target.nodeName === 'BUTTON') {
       if (input === 'AC') this.clearInput();
-      else if (numRegEx.test(parseInt(input))) this.addNum(input);
+      else if (numRegEx.test(parseFloat(input))) this.addNum(input);
       else if (input.match(operatorRegEx)) this.addOperator(input);
       else if (input === '.') this.addDecimal();
+      else if (input === '=') this.getResult();
     }
   }
 
+  // according to PEMDAS (elemtary school nostalgia!)
+  getResult = () => {
+    let tempArr = this.state.inputArr;
+    let newArr = [];
+    let result = 0;
+    let firstNum;
+    let secondNum;
+    let multidivregex = /x|\//ig;
+    let testForMultiplicatonOrDivision;
+
+    // loop through array and do all multiplicatons and divisions first
+    do {
+      for (let i = 0; i < tempArr.length-1; i++) {
+        firstNum = parseFloat(tempArr[i-1]);
+        secondNum = parseFloat(tempArr[i+1]);
+
+        if (tempArr[i] === '/' || tempArr[i] === 'x') {
+          // get result
+          if (tempArr[i] === '/') result = firstNum / secondNum;
+          else if (tempArr[i] === 'x') result = firstNum * secondNum;
+
+          // remove calculated numbers in array and save result instead
+          tempArr.splice(i - 1, 2);
+          tempArr[i - 1] = result.toString();
+        }
+      }
+
+      //check if any multiplications and/or divisions are left to repeat do while loop
+      testForMultiplicatonOrDivision = tempArr.map(currentItem => currentItem).filter(currentItem => currentItem.match(multidivregex));
+      // console.log(tempArr + ' ' + testForMultiplicatonOrDivision)
+    } while (testForMultiplicatonOrDivision.length > 0);
+
+    result = tempArr[0];
+    result = parseFloat(result);
+
+    // time to calculate additions and subtractions!
+    for (let i = 0; i < tempArr.length; i++) {
+      firstNum = parseFloat(tempArr[i + 1]);
+
+      if (tempArr[i] === '+') {
+        result = result + firstNum;
+      }
+      else if (tempArr[i] === '-') {
+        result = result - firstNum;
+      }
+
+
+    // check if result has a decimal number and formats output accordingly
+    }
+    if (Number.isInteger(result)) {
+      console.log('first func, num is' + result)
+      result = result.toString();
+      newArr[0] = result;
+    } 
+    else {
+      console.log(result)
+      result = parseFloat(result.toFixed(3));
+      console.log(result)
+      newArr[0] = result.toString();
+    }
+    this.setState({ inputArr: newArr })
+  }
+  
   clearInput = () => {
     this.setState({ inputArr: ['0'] })
   }
@@ -42,7 +105,6 @@ class App extends Component {
     let tempArr = this.state.inputArr;
     let currentPosition = this.state.inputArr.length - 1;
     const lastItem = tempArr.slice(-1)[0];
-    const currentNum = tempArr[currentPosition];
 
     if (this.state.inputArr.length === 1 && this.state.inputArr[0] === '0') {
       tempArr = ['0.']
@@ -62,7 +124,6 @@ class App extends Component {
     this.scrollToBottom();
   }
 
-  // function which adds entered numbers into state
   addNum = (num) => {
     const tempArr = this.state.inputArr;
     let currentPosition = this.state.inputArr.length - 1;
@@ -89,13 +150,11 @@ class App extends Component {
   addOperator = (operator) => {
     const tempArr = this.state.inputArr;
     const lastItem = tempArr.slice(-1)[0];
-    console.log(lastItem)
 
     if (this.state.inputArr[0] === '0') return;
     else if (lastItem.match(operatorRegEx)) return;
     else {
       tempArr.push(operator);
-      console.log(tempArr);
       this.setState({ inputArr: tempArr })
     }
 
